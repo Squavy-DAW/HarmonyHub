1f7144fc45e56b0524cf7eefb233f9d59ae332c9
Fabian Mild <fabian.mild@gmx.at>
2023-11-04 16:27:11 +0100

    just wanted to switch branches. This is not a real commit =)

--- src/synth/engine.ts
+++ src/synth/engine.ts
@@ -4,13 +4,9 @@ import { ADSREnvelope, createADSREnvelope } from "./envelope";
 let ctx:AudioContext;
 
 //creation pool
-export let osc1Params:OscillatorParams;
-export let osc2Params:OscillatorParams;
-export let osc3Params:OscillatorParams;
-export let osc1Envelope:ADSREnvelope;
-export let osc2Envelope:ADSREnvelope;
-export let osc3Envelope:ADSREnvelope;
-export let activeOscillators = [];
+export let oscillatorPresets:{id:string, params:OscillatorParams}[];
+export let activeOscillators:{id:string, params:OscillatorParams}[];
+export let activeEnvelopes:ADSREnvelope[] = [];
 
 //modulator pool
 
@@ -25,28 +21,34 @@ export interface AdvancedAudioNode{
 
 export function init(){
     //Initialize oscillators
-    osc1Params = setOscillatorParams("sine",1,0,0,0,0,0);
-    osc2Params = setOscillatorParams("sine",1,0,0,0,0,0);
-    osc3Params = setOscillatorParams("sine",1,0,0,0,0,0);
-    osc1Envelope = createADSREnvelope(ctx,0.1,1,0.3,0.5,3,0.2,0.1);
-    osc2Envelope = createADSREnvelope(ctx,0.1,1,0.3,0.5,3,0.2,0.1);
-    osc3Envelope = createADSREnvelope(ctx,0.1,1,0.3,0.5,3,0.2,0.1);
+    let osc1 = {id:"osc1", params:setOscillatorParams("sine",1,0,0,0,0,0)};
+    let osc2 = {id:"osc2", params:setOscillatorParams("sine",1,0,0,0,0,0)};
+    let osc3 = {id:"osc3", params:setOscillatorParams("sine",1,0,0,0,0,0)};
+    oscillatorPresets.push(osc1);
+    oscillatorPresets.push(osc2);
+    oscillatorPresets.push(osc3);
+    let generalEnvelope = createADSREnvelope(ctx,0.1,1,0.3,0.5,3,0.2,0.1);
+    activeEnvelopes.push(generalEnvelope);
 }
 
 
 //TODO: Make work...
 //start/stop
-export function start(freq:number, oscParams:OscillatorParams, envelope:ADSREnvelope){
-    let osc = setAdvancedOscillator(oscParams, ctx);
-    osc.osc().frequency.setValueAtTime(freq, ctx.currentTime);  //set the frequency
-    osc.connect(envelope);
+export function start(freq:number, oscId:string){
+    let oscPreset = oscillatorPresets.find(s => s.id==oscId);
+    if(oscPreset===undefined)
+        return;
+
+    //let osc = setAdvancedOscillator(oscPreset, ctx);
+    //osc.osc().frequency.setValueAtTime(freq, ctx.currentTime);  //set the frequency
+    
+    /*osc.connect(envelope);
     envelope.out.connect(ctx.destination);
     osc.osc().start();
-    envelope.trigger();
-
+    envelope.trigger();*/
 }
 
-export function stop(freq:number, oscParams:OscillatorParams, envelope:ADSREnvelope){
-    let osc = setAdvancedOscillator(oscParams, ctx);
-    envelope.stop();
+export function stop(freq:number, oscParams:OscillatorParams){
+    //let osc = setAdvancedOscillator(oscParams, ctx);
+    //envelope.stop();
 }
