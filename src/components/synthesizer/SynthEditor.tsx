3cb8276865eb5dc53d7c2a9f9888da82dd3a4107
m-ue-d <fabian.mild@gmx.at>
2023-11-15 00:23:06 +0100

    lines work with the svg solution. Keep the canvas solution in mind. If this would cause issues, we'll switch, but it would be too much work to be a top priority and we have no time so it will stay for now. And it's not bad either, rly. =)

--- src/components/synthesizer/SynthEditor.tsx
+++ src/components/synthesizer/SynthEditor.tsx
@@ -10,6 +10,7 @@ export default function SynthEditor(){
     const [nodes, setNodes] = useState<AudioNodeProps[]>([]);
     const { project } = useContext(ProjectContext);
     const [draggedNode, setDraggedNode] = useState<HTMLElement>();
+    const [hoverOverLine, setHoverOverLine] = useState<HTMLElement>();
     const nodeOrigin = useRef({x: 0, y: 0});
     const nodesDragOverlay = createRef<HTMLUListElement>();
     const { mousePosition, mouseDelta, mouseDown } = useMouse();
@@ -26,37 +27,80 @@ export default function SynthEditor(){
         setDraggedNode(target);
     }
 
-    function handleConnectionMouseDown(ev: React.MouseEvent){
+    function handleConnectionMouseClick(ev: React.MouseEvent){
+        ev.stopPropagation();
         const target = ev.currentTarget as HTMLElement;
         const {x,y} = target.getBoundingClientRect();
-        ev.stopPropagation();
         let offsetX = nodesDragOverlay.current!.getBoundingClientRect().x;
         let offsetY = nodesDragOverlay.current!.getBoundingClientRect().y;
-        setSvgDragLine({ 
+
+        if(_svgDragLine.current){
+            if(!svgLines.find(e => (e.a == _svgDragLine.current?.a && e.b == target.getAttribute("data-key")!)
+            || (e.a == target.getAttribute("data-key")! && e.b == _svgDragLine.current?.a)))
+                setSvgLines([...svgLines, { 
+                    x1: _svgDragLine.current.x1, 
+                    y1: _svgDragLine.current.y1, 
+                    x2: x - offsetX + target.clientWidth/2,
+                    y2: y - offsetY + target.clientHeight/2,
+                    a: _svgDragLine.current.a,
+                    b: target.getAttribute("data-key")!
+                }]);
+            _svgDragLine.current = undefined;
+            setSvgDragLine(undefined);
+            return;
+        }
+
+        _svgDragLine.current = { 
             x1: x - offsetX + target.clientWidth/2, 
             y1: y - offsetY + target.clientHeight/2, 
             x2: ev.clientX - offsetX, 
-            y2: ev.clientY - offsetY 
-        });
-        _svgDragLine.current = { x1: x + target.clientWidth/2, y1:y + target.clientWidth/2, x2: ev.clientX, y2: ev.clientY };
+            y2: ev.clientY - offsetY,
+            a: target.getAttribute("data-key")!,
+            b: undefined
+        };
+        setSvgDragLine(_svgDragLine.current);
     }
 
     useEffect(() => {
         if (!mouseDown && draggedNode) {
             draggedNode.style.rotate = '0deg';
-
             setDraggedNode(undefined);
             return;
         }
     }, [mouseDown])
 
     useEffect(() => {
+        if(mouseDown && hoverOverLine){
+            setSvgLines(svgLines.filter(x => x.a != hoverOverLine.getAttribute("data-a") || x.b != hoverOverLine.getAttribute("data-b")));
+        }
+
         if (mouseDown && draggedNode) {
-            const left = mousePosition.x - nodesDragOverlay.current!.getBoundingClientRect().x - nodeOrigin.current.x
-            const top = mousePosition.y - nodesDragOverlay.current!.getBoundingClientRect().y - nodeOrigin.current.y
+            let offsetX = nodesDragOverlay.current!.getBoundingClientRect().x;
+            let offsetY = nodesDragOverlay.current!.getBoundingClientRect().y;
+            const left = mousePosition.x - offsetX - nodeOrigin.current.x
+            const top = mousePosition.y - offsetY - nodeOrigin.current.y
             draggedNode.style.left = `${left}px`;
             draggedNode.style.top = `${top}px`;
             draggedNode.style.rotate = `${Math.min(Math.max(mouseDelta.x, -30), 30)}deg`;
+
+            //update lines
+            const connectors = Array.from(draggedNode.querySelectorAll(".audio-connection-node"));
+            svgLines.forEach((l,idx) => {
+                const connectorA = connectors.find(c => c.getAttribute('data-key') == l.a);
+                const connectorB = connectors.find(c => c.getAttribute('data-key') == l.b);
+                let newLines = [...svgLines];
+                if (connectorA) {
+                    const {x, y} = connectorA.getBoundingClientRect();
+                    newLines[idx].x1 = x - offsetX + connectorA.clientWidth/2;
+                    newLines[idx].y1 = y - offsetY + connectorA.clientHeight/2;
+                }
+                if (connectorB) {
+                    const {x, y} = connectorB?.getBoundingClientRect();
+                    newLines[idx].x2 = x - offsetX + connectorB.clientWidth/2;
+                    newLines[idx].y2 = y - offsetY + connectorB.clientHeight/2;
+                }
+                setSvgLines(newLines);
+            });
         }
     }, [mousePosition, mouseDelta])
 
@@ -65,23 +109,28 @@ export default function SynthEditor(){
         let offsetX = nodesDragOverlay.current!.getBoundingClientRect().x;
         let offsetY = nodesDragOverlay.current!.getBoundingClientRect().y;
         setSvgDragLine({
-            x1: _svgDragLine.current.x1 - offsetX, 
-            y1: _svgDragLine.current.y1 - offsetY, 
+            x1: _svgDragLine.current.x1, 
+            y1: _svgDragLine.current.y1, 
             x2: mousePosition.x - offsetX, 
-            y2: mousePosition.y - offsetY 
+            y2: mousePosition.y - offsetY,
+            a: _svgDragLine.current.a,
+            b: _svgDragLine.current.b
         });
     }, [mousePosition])
 
-
-
-    return (<section className="synth-editor">
+    return (<section className="synth-editor" onClick={
+        ()=>{
+            _svgDragLine.current = undefined;
+            setSvgDragLine(undefined);
+        }
+    }>
         <ul ref={nodesDragOverlay}>
                 {nodes?.map((node, i) =>
                     <li key={`node[${i}]`} className='audionode'
                         onMouseDown={handleNodeMouseDown}>
                         {node.name}
-                        <div className="audio-connection-node"
-                        onMouseDown={handleConnectionMouseDown}></div>
+                        <div className="audio-connection-node" data-key={`node-connector[${i}]`}
+                        onClick={handleConnectionMouseClick}></div>
                     </li>
                 )}
         </ul>
@@ -104,7 +153,14 @@ export default function SynthEditor(){
         <svg className="connection-lines" width="100%" height="100%">
             {svgDragLine && <line className="dragged-connection-line" x1={svgDragLine.x1} y1={svgDragLine.y1} x2={svgDragLine.x2} y2={svgDragLine.y2} stroke="black" strokeWidth="5"/>}
             {svgLines.map((l,i) => (
-                <line key={`connection-line[${i}]`} x1={l.x1} y1={l.y1} x2={l.x2} y2={l.y2} stroke="black" strokeWidth="5"/>
+                <line key={`connection-line[${i}]`} x1={l.x1} y1={l.y1} x2={l.x2} y2={l.y2} stroke="black" strokeWidth="5" data-a={l.a} data-b={l.b}
+                className="placed-line"
+                onMouseEnter={(e)=>{
+                    setHoverOverLine(e.target as HTMLElement);
+                }}
+                onMouseLeave={(e)=>{
+                    setHoverOverLine(undefined);
+                }}/>
             ))}
         </svg>
         
--- src/model/linepositionprops.ts
+++ src/model/linepositionprops.ts
@@ -2,5 +2,7 @@ export default interface LinePosition {
     x1: number,
     y1: number,
     x2: number,
-    y2: number
+    y2: number,
+    a: string|undefined,
+    b: string|undefined
 }
