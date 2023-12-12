daeb091bbd7ed71fa65b7c6933e1bc18fb3ec1c5
LambdaSpg <lambda-spg@protonmail.com>
2023-12-12 13:03:14 +0100

    Changed the name to 'SquidWave' on the Home screen

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -97,7 +97,7 @@ export default function Home() {
             <AsciiLogo />
 
             <main id="home-title">
-                <h1>Welcome to HarmonyHub!</h1>
+                <h1>Welcome to SquidWave!</h1>
             </main>
 
             <main id="home-controls">
