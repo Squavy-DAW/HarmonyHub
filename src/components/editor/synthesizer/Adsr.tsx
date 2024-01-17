d7a31d74249b253746b563dcc96410fe34b12702
Fabian Hummel <hum22501@spengergasse.at>
2024-01-17 19:49:53 +0100

    Adsr background image

---
+++ src/components/editor/synthesizer/Adsr.tsx
@@ -0,0 +1,9 @@
+import styles from "@styles/synthesizer/Adsr.module.css"
+
+export default function Adsr() {
+    return (
+        <div className={styles.container}>
+            
+        </div>
+    )
+}
